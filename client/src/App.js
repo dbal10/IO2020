793320328fcce2d46954d73b952ec5badca0f9d8
224247 Andryskowski Stanisław <224247@edu.p.lodz.pl>
2020-12-21 16:58:45 +0100

    added routing, view1 component, react-router-dom, changed app.js,

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,24 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import FirstConnect from './components/FirstConnect/FirstConnect';
 import Firebase from "./components/Firebase/Firebase";
+import View1 from "./Admin/View1";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+
+    <Switch>
+      <Route path="/View1" component = {View1}/>
+    </Switch>
+
     <BrowserRouter>
       <div className="App">
         <FirstConnect />
         <Firebase/>
       </div>
     </BrowserRouter>
+    </Router>
   );
 }
 
