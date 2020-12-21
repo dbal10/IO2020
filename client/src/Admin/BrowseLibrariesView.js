5cca563ca9f56b49f0513f3665ab9d6ca8aa41dc
Domziel <dominix356@gmail.com>
2020-12-21 16:38:11 +0100

    widok do wyboru biblioteki do przejrzenia

---
+++ client/src/Admin/BrowseLibrariesView.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import AccountInfo from './AccountInfo'
+import './style.css'
+
+class BrowseLibrariesView extends Component{
+    render() {
+        return (
+            <div>
+                <AccountInfo account='Dane Konta'/>
+                <button type="button" className="libraryButton" style={{backgroundColor: "#4CAF50"}} onClick={this.onMapTemplatesLibraryButtonCllick}>Map templates library</button>
+                <button type="button" className="libraryButton" style={{backgroundColor: "#008CBA"}} onClick={this.onItemsLibraryButtonCllick}>Items library</button>
+                <button type="button" className="libraryButton" style={{backgroundColor: "#F44336"}} onClick={this.onUsersMapsLibraryButtonCllick}>Users' maps library</button>
+            </div>
+        )
+    }
+
+    onMapTemplatesLibraryButtonCllick(){
+        console.log("przycisk1")
+    }
+
+    onItemsLibraryButtonCllick(){
+        console.log("przycisk2")
+    }
+
+    onUsersMapsLibraryButtonCllick(){
+        console.log("przycisk3")
+    }
+}
+
+export default BrowseLibrariesView
