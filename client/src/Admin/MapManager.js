b50e5ffbab4a58182618baa55ca5754efb815a51
Domziel <dominix356@gmail.com>
2020-12-21 21:08:55 +0100

    MapManager i metodki save, get, delete

---
+++ client/src/Admin/MapManager.js
@@ -0,0 +1,36 @@
+import fire from '../firebase';
+
+class MapManager {
+
+    saveMap(obj) {
+        let id = obj.id.toString().valueOf();
+        fire.database().ref('database/maps').child(id).set(obj);
+    }
+
+    getMapById(id){
+        let val;
+        fire.database().ref(`database/maps`).child(id)
+        .on("value", snapshot => { 
+            val = snapshot.val()
+        })
+        return val
+    }
+
+    deleteMap(id){
+        fire.database().ref(`database/maps/${id}`).remove();
+    }
+
+    getAllMaps(){
+        let arr = []
+        fire.database().ref("database/maps")
+        .on("value", snapshot => {
+            snapshot.forEach(snap => {
+                arr.push(snap.val())
+            })
+        })
+        console.log(arr)
+        return arr
+    }
+}
+
+export default MapManager
